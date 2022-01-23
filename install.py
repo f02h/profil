55f4f5b53e958e2a075c82f7770e43276a72b648
f02h <Preeleesje42>
2022-01-23 12:22:45 +0100

    1

--- install.py
+++ install.py
@@ -5,5 +5,5 @@ conn.execute("CREATE TABLE vrtalka (id INTEGER PRIMARY KEY, name char(100) NOT N
 conn.execute("CREATE TABLE profili (id INTEGER PRIMARY KEY, name char(100) NOT NULL)")
 conn.execute("INSERT INTO profili (name) VALUES ('profil_20x20'),('profil_30x30')")
 conn.execute("CREATE TABLE vars (id INTEGER PRIMARY KEY, name char(100) NOT NULL, value REAL NOT NULL, idProfil INTEGER)")
-conn.execute("INSERT INTO vars (name, value) VALUES ('pozicijaLNull',10.0,1),('pozicijaDNull',10.0,1),('pozicijaL',10.0,1),('pozicijaD',10.0,1),('orodjeL',10.0,1),('orodjeD',10.0,1),('hodL',10.0,1),('pocasnejePredKoncemHodaL',10.0,1),('hitrostPredKoncemHodaL',10.0,1),('hodD',10.0,1),('pocasnejePredKoncemHodaD',10.0,1),('hitrostPredKoncemHodaD',10.0,1),('povratekL',10.0,1),('povratekD',10.0,1),('povrtavanjeL',10.0,1),('povrtavanjeD',10.0,1)")
+conn.execute("INSERT INTO vars (name, value, idProfil) VALUES ('pozicijaLNull',10.0,1),('pozicijaDNull',10.0,1),('pozicijaL',10.0,1),('pozicijaD',10.0,1),('orodjeL',10.0,1),('orodjeD',10.0,1),('hodL',10.0,1),('pocasnejePredKoncemHodaL',10.0,1),('hitrostPredKoncemHodaL',10.0,1),('hodD',10.0,1),('pocasnejePredKoncemHodaD',10.0,1),('hitrostPredKoncemHodaD',10.0,1),('povratekL',10.0,1),('povratekD',10.0,1),('povrtavanjeL',10.0,1),('povrtavanjeD',10.0,1)")
 conn.commit()
